12b4919c40a3a28b65c222e64f6f39f8c3ba4200
Nyantemafy <antema.fy01@gmail.com>
2025-12-16 17:55:23 +0300

    configuration backend

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: ['https://nyantemafy.github.io', 'http://localhost:3000']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -57,7 +59,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
